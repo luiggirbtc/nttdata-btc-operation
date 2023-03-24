c2b399e512d1154cea45b5d81d017678179f7c7d
brayanlrs28 <riverasantiagobrayan@gmail.com>
2023-03-24 01:04:16 -0500

    update remove innecesary dependency checkstyle

--- src/main/java/com/nttdata/btc/operation/app/model/request/BaseRequest.java
+++ src/main/java/com/nttdata/btc/operation/app/model/request/BaseRequest.java
@@ -1,13 +1,11 @@
 package com.nttdata.btc.operation.app.model.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
